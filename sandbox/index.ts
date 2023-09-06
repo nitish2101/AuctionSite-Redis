eaddcae81c0275cc9cf8dca6df497b7ba01c2dc6
Nitish Vashisht <nitishsharma50@gmail.com>
2023-09-06 13:09:15 +0530

    starter

---
+++ sandbox/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { client } from '../src/services/redis';
+
+const run = async () => {
+	await client.hSet('car', 'color', 'red');
+
+	const car = await client.hGetAll('car');
+	console.log(car);
+};
+run();
